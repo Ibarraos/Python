8e03ade01d749fd45ce8af221199f40f9c5d71d3
Osvaldo Ibarra <ibarraosvaldo7@gmail.com>
2022-11-07 20:21:29 -0700

    python files

---
+++ Activites/pythontest.py
@@ -0,0 +1,8 @@
+## This is a comment line 
+##(This line will be ignored but will help talking about code)
+
+## can use "" or ''
+print('Hello World!')
+
+## To run code can use gitbash using command 'python 'file''
+##
